ce38c4f2b8146d50b9d5cc31a2a3cac65c80f620
Yonghui Zhao <zhaoyh@rd.netease.com>
2017-08-30 10:20:45 +0800

    MOD:main

--- src/main/java/com/hts/htstask/HtsTaskApplication.java
+++ src/main/java/com/hts/htstask/HtsTaskApplication.java
@@ -2,11 +2,15 @@ package com.hts.htstask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HtsTaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HtsTaskApplication.class, args);
+		System.out.println("Start Task..........");
 	}
+
 }
