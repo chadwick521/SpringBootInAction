7bf386e06cd5cef3a42ebd816bdf82938b5f21f6
Yonghui Zhao <zhaoyh@rd.netease.com>
2017-08-30 10:14:38 +0800

    ADD:嵌入项目

---
+++ src/main/java/com/hts/htstask/HtsTaskApplication.java
@@ -0,0 +1,12 @@
+package com.hts.htstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtsTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtsTaskApplication.class, args);
+	}
+}
